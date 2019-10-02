ab452f2d004d5bd19316d7819c68b3fc7a82928f
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-10-01 17:45:08 -0700

    Start implementing std::pair but I need type traits first

--- stdutility.h
+++ stdutility.h
@@ -21,5 +21,13 @@
 // basic utility implementation
 
 namespace std {
+    template<typename T1, typename T2>
+    struct pair {
+        using first_type = T1;
+        using second_type = T2;
 
-}
+
+        first_type first;
+        second_type second;
+    };
+} // end namespace std
